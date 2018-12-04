fcc3b42260ee1d0dd85a66a83164a4f5eccf70db
Sebasinos <42095443+Sebasinos@users.noreply.github.com>
2018-12-04 11:31:34 -0300

    Update
    se traspasa a función y cambian el nombre de algunas variables.

--- src/Decaimiento.py
+++ src/Decaimiento.py
@@ -1,8 +1,9 @@
 import math
-#Aqui hay que pedir los datos
-Act_ini = float(raw_input("Ingrese Actividad Inicial: "))
-tiempo = float(raw_input("Ingrese tiempo: "))
-des_fluor =float(109.771)
-Act_fin =Act_ini*(math.exp(-(0.693*float(tiempo)/des_fluor)))
-
-print "El decaimiento es", Act_fin
+#Se transforma a funcion , para recibir los parametros desde los modulos que realizan check de los datos.
+def cal_decay(dose_act,minut_decay):
+    dose_act=float(dose_act)
+    des_fluor =float(109.771)
+    minutos=float(minut_decay)
+    act_fin=dose_act*(math.exp(-(0.693*minutos/des_fluor)))
+   
+    return act_fin
