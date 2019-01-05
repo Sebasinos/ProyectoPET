86354263c9cc7c74529ce7f828ffa9cbac5f7863
Sebasinos <sebas.inos@gmail.com>
2019-01-05 18:08:40 -0300

    module and doc - fx modify data radiopharmaceutical

--- src/input_data_mod.py
+++ src/input_data_mod.py
@@ -1,54 +1,15 @@
-#FUNCION MODIFICACION ULTIMO PACIENTE INGRESADO
-def mod_last_dat(lista2,lista):
-    if (lista2 == []):
-        print("No hay pacientes para modificar")
-
-    else:
-        opc=0
-        while opc != "s":
-            print("Ingrese una opcion valida")
-            menuPrincipal = menu_mod()
-            opc = opciones()
-            
-            if(opc == "1"):
-                dose=check_input_dose()
-                lis=list(lista2[-1])
-                lis[0]=dose
-                hour=lis[1]
-                ml=lis[2]
-                listanew=tuple(lis)
-                lista2.pop(-1)
-                lista2.append(listanew)
-                lista.pop(-1)
-                input_data_mod(dose,hour,ml)
-                print ("Regresando al Menu")
-
-            if(opc == "2"):
-                hour=check_input_hora()
-                lis=list(lista2[-1])
-                lis[1]=hour
-                dose=lis[0]
-                ml=lis[2]
-                listanew=tuple(lis)
-                lista2.pop(-1)
-                lista2.append(listanew)
-                lista.pop(-1)
-                input_data_mod(dose,hour,ml)
-                print ("Regresando al Menu")
-
-
-            if(opc == "3"):
-                ml=check_input_ml()
-                lis=list(lista2[-1])
-                lis[2]=ml
-                dose=lis[0]
-                hour=lis[1]
-                listanew=tuple(lis)
-                lista2.pop(-1)
-                lista2.append(listanew)
-                lista.pop(-1)
-                input_data_mod(dose,hour,ml)
-                print ("Regresando al Menu")
-        
-
-
+#FUNCION ACTUALIZACION VALORES DE RADIOFARMACO TRAS MODIFICACION
+def input_data_mod(dose,hour,ml):
+    act_dose=dose_last(lista)
+    act_ml=ml_last(lista)
+    act_hour=time_last(lista)
+    new_dose=dose
+    new_hour=hour
+    new_ml=ml
+    minutos=dif_min_proy(act_hour,new_hour)
+    doserefresh=cal_decay(act_dose,minutos)
+    doserefresh=round(float(doserefresh)-float(new_dose),2)
+    mlrefresh= float(ml_last(lista)) - float(new_ml)
+    tupla_act=(str(doserefresh), new_hour, str(mlrefresh))
+    lista.append(tupla_act)
+    print ("Datos ingresados con exito")
